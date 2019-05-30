a2c6412675849317d78599949ef77fad51ddac7b
Alex Nazarov <pokupka91@mail.ru>
2019-05-30 12:02:35 +0300

    update add_do_card function

--- public/app3.js
+++ public/app3.js
@@ -12,9 +12,9 @@ function something()
 
 function add_to_card(id)
 {
-	alert('id pizza: '+ id);
+	var key = 'product_' + id;
 
-
-
-
+	var x = window.localStorage.getItem(key);
+	x = x * 1 + 1;
+	window.localStorage.setItem(key, x);
 }
