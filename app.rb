1dbecaab6b2e6f5750b17f3c9fa8da98c41d410b
Alex Nazarov <pokupka91@mail.ru>
2019-05-31 09:29:03 +0300

    add cart view

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
+require './lib/my_procedures'
 
 set :database, "sqlite3:pizzashop.db" 
 
@@ -21,23 +22,6 @@ end
 post '/cart' do
 	orders_input = params[:orders]
 	@orders = parse_orders_line orders_input
-  erb "Hello World #{@orders.inspect}"
+  erb :cart
 end
 
-def parse_orders_line orders_input
-	s1 = orders_input.split(/,/)
-
-	arr = []
-
-	s1.each do |x|
-	  s2 = x.split(/=/)
-	  s3 = s2[0].split(/_/)
-	  id = s3[1]
-	  cnt = s2[1]
-
-		arr2 = [id, cnt]
-		arr.push arr2
-	end
-
-	return arr
-end
